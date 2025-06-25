3551ba0492be15680082669091f3f4b98587129a
nat1han <74563313+nat1han@users.noreply.github.com>
2025-06-25 22:38:08 +0300

    Update run_array.sh

--- run_array.sh
+++ run_array.sh
@@ -2,18 +2,14 @@
 #PBS -N Popov_integral_array
 #PBS -l walltime=00:02:00
 #PBS -l nodes=1:ppn=1
-#PBS -t 0-3 
+#PBS -t 0-3
 #PBS -j oe
 #PBS -o output_log_$PBS_ARRAYID.txt
 
 cd $PBS_O_WORKDIR
 
 line=$(sed -n "$((PBS_ARRAYID + 1))p" input.txt)
-xmin=$(echo $line | awk '{print $1}')
-xmax=$(echo $line | awk '{print $2}')
-
-ymin=0.0
-ymax=3.0
+read xmin xmax ymin ymax <<< "$line"
 
 g++ -o integral integral.cpp -lm
 
