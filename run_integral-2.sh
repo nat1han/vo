055896e9efd510ad6930dc3c5990df982c43cf84
nat1han <74563313+nat1han@users.noreply.github.com>
2025-06-25 22:47:40 +0300

    Update run_integral-2.sh

--- run_integral-2.sh
+++ run_integral-2.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-#PBS -N Popov_integral
+#PBS -N Popov_integral_array
 #PBS -l walltime=00:02:00
 #PBS -l nodes=1:ppn=1
+#PBS -t 0-3                    
 #PBS -j oe
-#PBS -o output_log.txt
+#PBS -o output_log_$PBS_ARRAYID.txt
 
 cd $PBS_O_WORKDIR
 
-if [ ! -f input.txt ]; then
-  echo "Файл input.txt не знайдено!" >&2
-  exit 1
-fi
-
-read xmin xmax < input.txt
+# Отримати відповідний рядок із input.txt
+line=$(sed -n "$((PBS_ARRAYID + 1))p" input.txt)
+xmin=$(echo $line | awk '{print $1}')
+xmax=$(echo $line | awk '{print $2}')
 
 ymin=0.0
 ymax=3.0
 
 g++ -o integral integral.cpp -lm
 
-./integral "$xmin" "$xmax" "$ymin" "$ymax" | grep RESULT | awk '{print $2}' > output.txt
+./integral "$xmin" "$xmax" "$ymin" "$ymax" | grep RESULT | awk '{print $2}' > output_$PBS_ARRAYID.txt
